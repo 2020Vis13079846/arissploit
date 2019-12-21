197432280daef1f748ed3c1c7f7347e186ab785a
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-21 19:43:16 +0300

    Update header.py

--- core/header.py
+++ core/header.py
@@ -15,7 +15,7 @@ def print_info():
 	count()
 
 	print("\t" + colors.bold + "    Arissploit Framework\n" + colors.end)
-	print("\t" + colors.bold + "Developer "+colors.end+"\t[ "+info.author+" ]" + colors.end)	
+	print("\t" + colors.bold + "Author "+colors.end+"\t[ "+info.author+" ]" + colors.end)	
 	print("\t" + colors.bold + "Version"+colors.end+"\t[ "+info.version+"          ]"+colors.end)
 	print("\t" + colors.bold + "Modules "+colors.end+"[ "+count.mod+" modules"+"        ]"+colors.end)
 	print("\n")

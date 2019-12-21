778b5008a758eaa9171d3ff75d8e1c88acd2c9fb
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-21 19:45:23 +0300

    Update header.py

--- core/header.py
+++ core/header.py
@@ -3,7 +3,6 @@ from core import colors
 from core.moduleop import count
 
 arissploit = r"""
-
                 .----.   @   @                  
                / .-.-.`.  \v/                            
                | | '\ \ \_/ )                      
@@ -17,5 +16,5 @@ def print_info():
 	print("\t" + colors.bold + "    Arissploit Framework\n" + colors.end)
 	print("\t" + colors.bold + "Author "+colors.end+"\t[ "+info.author+" ]" + colors.end)	
 	print("\t" + colors.bold + "Version"+colors.end+"\t[ "+info.version+"            ]"+colors.end)
-	print("\t" + colors.bold + "Modules "+colors.end+"[ "+count.mod+" modules"+"    ]"+colors.end)
-	print("\n")
+	print("\t" + colors.bold + "Modules "+colors.end+"[ "+count.mod+" modules"+"     ]"+colors.end)
+	print("")

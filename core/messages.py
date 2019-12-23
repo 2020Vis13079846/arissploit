c4fcd7a371a7ab904eaefc83e786e928739fe353
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-23 10:32:46 +0100

    Update messages.py

--- core/messages.py
+++ core/messages.py
@@ -4,7 +4,7 @@ from core import colors
 errorstr = "["+colors.bold+colors.red+"err"+colors.end+"] "
 infostr = "["+colors.bold+colors.blue+"inf"+colors.end+"] "
 warningstr = "["+colors.bold+colors.yellow+"war"+colors.end+"] "
-successstr = "["+colors.bold+colors.green+"suf"+colors.end+"] "
+successstr = "["+colors.bold+colors.green+"suc"+colors.end+"] "
 
 def printError(message, start="", end="\n"):
 	sys.stdout.write(errorstr+message+end)

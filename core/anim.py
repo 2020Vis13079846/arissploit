81164bb4d0b5318bad20caca05116d5d57dd9756
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-25 14:59:41 +0100

    Update anim.py

--- core/anim.py
+++ core/anim.py
@@ -4,7 +4,7 @@ from core import colors
 errorstr = "["+colors.bold+colors.red+"err"+colors.end+"] "
 infostr = "["+colors.bold+colors.blue+"inf"+colors.end+"] "
 warningstr = "["+colors.bold+colors.yellow+"war"+colors.end+"] "
-successstr = "["+colors.bold+colors.green+"suf"+colors.end+"] "
+successstr = "["+colors.bold+colors.green+"suc"+colors.end+"] "
 
 def animline(message, last=False):
 	sys.stdout.write("\033[K")

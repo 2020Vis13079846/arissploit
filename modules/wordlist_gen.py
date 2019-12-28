1c91053fb1ce13396a471a99db8db11f914348e2
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-28 13:07:17 +0100

    Update wordlist_gen.py

--- modules/wordlist_gen.py
+++ modules/wordlist_gen.py
@@ -63,10 +63,7 @@ class Worker(threading.Thread):
 
 	def run(self):
 		try:
-			if not '/' in variables["output"][0]:
-				f = open(os.environ['OLDPWD'] + '/' + variables["output"][0], "a")
-			else:
-				f = open(variables["output"][0], "a")
+			f = open(variables["output"][0], "a")
 			
 		except Exception as error:
 			printError(error)

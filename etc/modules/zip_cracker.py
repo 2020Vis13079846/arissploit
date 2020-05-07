ce1cb6b0511032b5da6ee1244d75011ea831d8e1
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-05-07 11:35:50 +0300

    Update zip_cracker.py

--- etc/modules/zip_cracker.py
+++ etc/modules/zip_cracker.py
@@ -39,7 +39,7 @@ conf = {
 
 # List of the variables
 variables = OrderedDict((
-	("file", ["", "Target zip file."]),
+	("file", ["", "Input zip file."]),
 	("dict", ["", "Dictionary of words."]),
 	("tc", [8, "Thread count."]),
 	("exto", ["", "Extract directory."])
@@ -89,10 +89,10 @@ class Worker(threading.Thread):
 			else:
 				zipf = zipfile.ZipFile(file)
 		else:
-			printError("Local file: "+file+": does not exist!")
+			printError("Input file: "+file+": does not exist!")
 			g = os.environ['HOME']
 			os.chdir(g + "/arissploit")
-			return ModuleError("Local file: "+file+": does not exist!")
+			return ModuleError("Input file: "+file+": does not exist!")
 		g = os.environ['HOME']
 		os.chdir(g + "/arissploit")
 		
@@ -134,10 +134,10 @@ def run():
 		else:
 			wordlist = open(dicti, "rb")
 	else:
-		printError("Local file: "+dicti+": does not exist!")
+		printError("Input file: "+dicti+": does not exist!")
 		g = os.environ['HOME']
 		os.chdir(g + "/arissploit")
-		return ModuleError("Local file: "+dicti+": does not exist!")
+		return ModuleError("Input file: "+dicti+": does not exist!")
 	g = os.environ['HOME']
 	os.chdir(g + "/arissploit")
 	
